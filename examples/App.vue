6d254ddf846a63d0a2a3db11bf26ea419c843b90
pwndex <pwndex42@gmail.com>
2020-08-27 19:22:33 +0500

    feat: add spinners examples

---
+++ examples/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Ring />
+    <CubeGrid />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Ring, CubeGrid } from '@/index'
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Ring,
+    CubeGrid,
+  },
+})
+</script>
