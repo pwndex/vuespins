4490d0bac5c86ee28a1c881932aa87d2bf8fb79e
pwndex <pwndex42@gmail.com>
2020-08-30 15:54:19 +0500

    feat: add spinner component

---
+++ src/components/Spinner/Spinner.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="spinner" :style="cssVars">
+    <div class="dot1"></div>
+    <div class="dot2"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: {
+    color: {
+      type: String,
+      default: '#333',
+    },
+    size: {
+      type: String,
+      default: '50',
+    },
+  },
+  computed: {
+    cssVars() {
+      return {
+        '--vcs-spinner-size': this.size + 'px',
+        '--vcs-spinner-color': this.color,
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.spinner {
+  width: var(--vcs-spinner-size);
+  height: var(--vcs-spinner-size);
+  position: relative;
+  -webkit-animation: sk-rotate 2s infinite linear;
+  animation: sk-rotate 2s infinite linear;
+}
+
+.dot1,
+.dot2 {
+  width: 60%;
+  height: 60%;
+  display: inline-block;
+  position: absolute;
+  top: 0;
+  background-color: var(--vcs-spinner-color);
+  border-radius: 100%;
+  -webkit-animation: sk-bounce 2s infinite ease-in-out;
+  animation: sk-bounce 2s infinite ease-in-out;
+}
+
+.dot2 {
+  top: auto;
+  bottom: 0;
+  -webkit-animation-delay: -1s;
+  animation-delay: -1s;
+}
+
+@-webkit-keyframes sk-rotate {
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+@keyframes sk-rotate {
+  100% {
+    transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+  }
+}
+
+@-webkit-keyframes sk-bounce {
+  0%,
+  100% {
+    -webkit-transform: scale(0);
+  }
+  50% {
+    -webkit-transform: scale(1);
+  }
+}
+
+@keyframes sk-bounce {
+  0%,
+  100% {
+    transform: scale(0);
+    -webkit-transform: scale(0);
+  }
+  50% {
+    transform: scale(1);
+    -webkit-transform: scale(1);
+  }
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Ring } from './Ring'
 export { default as CubeGrid } from './CubeGrid'
+export { default as Spinner } from './Spinner'
