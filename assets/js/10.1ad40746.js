5cc3d1f618af3392e70f12d423c752675d979b5b
pwndex <pwndex42@gmail.com>
2020-09-08 02:59:32 +0500

    Updates

---
+++ assets/js/10.1ad40746.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{356:function(n,e,t){"use strict";t.r(e);var r={name:"app",components:{Spinner:t(100).c}},s=t(45),c=Object(s.a)(r,(function(){var n=this.$createElement,e=this._self._c||n;return e("div",[e("Spinner",{attrs:{color:"#fb503b",size:"80"}})],1)}),[],!1,null,null,null);e.default=c.exports}}]);
---
+++ assets/js/7.6f3dd144.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{357:function(n,e,t){"use strict";t.r(e);var i={name:"app",components:{Ring:t(100).b}},s=t(45),a=Object(s.a)(i,(function(){var n=this.$createElement,e=this._self._c||n;return e("div",[e("Ring")],1)}),[],!1,null,null,null);e.default=a.exports}}]);
