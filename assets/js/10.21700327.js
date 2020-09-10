55cb1f82eb6c59d76262541b58c92da8697ab72b
pwndex <pwndex42@gmail.com>
2020-09-10 13:25:08 +0500

    Updates

--- assets/js/10.21700327.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{349:function(n,e,t){"use strict";t.r(e);var r={name:"app",components:{Spinner:t(91).c}},s=t(40),c=Object(s.a)(r,(function(){var n=this.$createElement,e=this._self._c||n;return e("div",[e("Spinner",{attrs:{color:"#fb503b",size:"80"}})],1)}),[],!1,null,null,null);e.default=c.exports}}]);
