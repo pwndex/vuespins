10b546cf042ec4946852bc24eb5bfc2843d344f8
pwndex <pwndex42@gmail.com>
2020-09-24 17:04:46 +0500

    refactor: update examples vcli

--- examples/vcli/src/App.vue
+++ examples/vcli/src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    <Ring color="#333" size="50" thickness="3" speed="5" />
-    <CubeGrid color="#333" size="50" />
-    <Spinner color="#333" size="50" />
+    <Ring color="#ffffff" size="50" thickness="3" speed="2" />
+    <CubeGrid color="#ffffff" size="50" />
+    <Spinner color="#ffffff" size="50" speed="2" />
+    <Square color="#ffffff" size="50" speed="2" />
+    <Chase color="#ffffff" size="50" speed="2" />
+    <Pulse color="#ffffff" size="50" speed="2" />
   </div>
 </template>
 
@@ -13,10 +16,20 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background: #000000;
+  height: 100vh;
+}
+
 #app {
   margin-top: 10px;
   display: flex;
   justify-content: space-between;
-  width: 200px;
+  width: 500px;
 }
 </style>
